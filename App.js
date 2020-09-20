c5fa66fc854bab369b2864c9e9231af8eac28a5d
cquintos <jcarlo.quintos@gmail.com>
2020-09-21 07:02:42 +0800

    first commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Initialize</Text>
+      <Text>Try try</Text>
       <StatusBar style="auto" />
     </View>
   );
